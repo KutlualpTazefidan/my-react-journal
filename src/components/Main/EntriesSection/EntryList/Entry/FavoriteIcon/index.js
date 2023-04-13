b1888f91289bc95c897a4968b58f07fbbd3662c8
= <=>
2023-04-13 23:36:23 +0200

    Toggle favorite star

--- src/components/Main/EntriesSection/EntryList/Entry/FavoriteIcon/index.js
+++ src/components/Main/EntriesSection/EntryList/Entry/FavoriteIcon/index.js
@@ -1,10 +1,18 @@
 import "./FavoriteIcon.css";
-
-export default function FavoriteIcon({ active }) {
-  let imageSource = "/star.svg";
-  if (active) imageSource = "/star-filled.svg";
+import { useState } from "react";
+export default function FavoriteIcon() {
+  // let imageSource = "/star.svg";
+  // if (active) imageSource = "/star-filled.svg";
+  function activateFavoriteStart() {
+    changeStatus(!active);
+    active
+      ? changeImageSource("/star-filled.svg")
+      : changeImageSource("/star.svg");
+  }
+  const [imageSource, changeImageSource] = useState("/star.svg");
+  const [active, changeStatus] = useState(true);
   return (
-    <button className="favorite-button">
+    <button onClick={activateFavoriteStart} className="favorite-button">
       <img src={imageSource} alt="favorite" />
     </button>
   );
